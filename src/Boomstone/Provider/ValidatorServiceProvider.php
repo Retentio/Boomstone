d984a35b62993994a0016848e5f40d93230b2870
Ludovic Fleury <ludo.fleury@gmail.com>
2012-03-16 17:57:35 +0100

    Updates licence.

--- src/Boomstone/Provider/ValidatorServiceProvider.php
+++ src/Boomstone/Provider/ValidatorServiceProvider.php
@@ -1,9 +1,11 @@
 <?php
 
-/*
- * This file is part of the Silex framework.
+/**
+ * This file is part of the Boomstone PHP Silex boilerplate.
  *
- * (c) Fabien Potencier <fabien@symfony.com>
+ * https://github.com/Retentio/Boomstone
+ *
+ * (c) Ludovic Fleury <ludo.fleury@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
